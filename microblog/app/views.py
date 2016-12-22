5eb0fe6ecab296327606582b1540cb28ac42bcf8
yangr5 <yangtao584@126.com>
2016-12-22 21:42:08 +0800

    update on 2016.12.22 for data

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,8 +1,28 @@
 from flask import render_template
 from app import app
 import xlrd
-book = xlrd.open_workbook("test.xlsx")
+book = xlrd.open_workbook("data.xlsx")
 
+def getData(sheetName):
+    sh = book.sheet_by_name(sheetName)
+    num_rows = sh.nrows
+    num_cols = sh.ncols
+    num_rows = sh.nrows
+    posts={}
+    posts['body']=[]
+    posts['heads']=[]
+    for curr_row in range(num_rows):
+        post={}
+        
+        row = sh.row_values(curr_row)
+        if(curr_row !=0):
+            post = dict(zip(heads,row))
+            posts['body'].append(post)
+            
+        else:
+            heads=row
+            posts['heads'] = row
+    return posts
 @app.route('/')
 @app.route('/home')
 def home():
@@ -10,44 +30,46 @@ def home():
 
 @app.route('/array')
 def array():
-    sh = book.sheet_by_name("Sheet1")
-    num_rows = sh.nrows
-    num_cols = sh.ncols
-    num_rows = sh.nrows
-    posts={}
-    posts['body']=[]
-    posts['heads']=[]
-    for curr_row in range(num_rows):
-        post={}
-        
-        row = sh.row_values(curr_row)
-        if(curr_row !=0):
-            post = dict(zip(heads,row))
-            posts['body'].append(post)
-            
-        else:
-            heads=row
-            posts['heads'] = row
-    return render_template("array.html",title = 'array',posts = posts)
+    return render_template("data.html",posts = getData(sheetName = "Array"))
 
-@app.route('/score')
-def score():
-    sh = book.sheet_by_name("Sheet2")
-    num_rows = sh.nrows
-    num_cols = sh.ncols
-    num_rows = sh.nrows
-    posts={}
-    posts['body']=[]
-    posts['heads']=[]
-    for curr_row in range(num_rows):
-        post={}
-        
-        row = sh.row_values(curr_row)
-        if(curr_row !=0):
-            post = dict(zip(heads,row))
-            posts['body'].append(post)
-            
-        else:
-            heads=row
-            posts['heads'] = row
-    return render_template("array.html",title = 'score',posts = posts)
+@app.route('/host')
+def host():
+    return render_template("data.html",posts = getData(sheetName = "Host"))
+
+@app.route('/virtualization')
+def virtualization():
+    return render_template("data.html",posts = getData(sheetName = "Virtualization"))
+
+@app.route('/falcon 404 IP assignment')
+def falconIPAssignment():
+    return render_template("data.html",posts = getData(sheetName = "Falcon 404 IP assignment"))
+    
+@app.route('/rockies')
+def rockies():
+    return render_template("data.html",posts = getData(sheetName = "Rockies"))
+    
+    
+@app.route('/sanblaze')
+def sanblaze():
+    return render_template("data.html",posts = getData(sheetName = "Sanblaze"))
+
+    
+@app.route('/service')
+def service():
+    return render_template("data.html",posts = getData(sheetName = "Service"))    
+
+@app.route('/daily tool')
+def dailyTool():
+    return render_template("data.html",posts = getData(sheetName = "Daily tool"))
+    
+@app.route('/special setup')
+def specialSetup():
+    return render_template("data.html",posts = getData(sheetName = "Special setup"))
+    
+@app.route('/standard tb')
+def standardTB():
+    return render_template("data.html",posts = getData(sheetName = "Standard TB"))
+    
+@app.route('/baseline tc')
+def baselineTC():
+    return render_template("data.html",posts = getData(sheetName = "Baseline TC"))
