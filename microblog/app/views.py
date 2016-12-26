6e0fced1822664224086099fb6dfbd9656f9ca1d
yangr5 <yangtao584@126.com>
2016-12-26 17:53:16 +0800

    add upload file.

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -4,9 +4,9 @@ from werkzeug.utils import secure_filename
 from app import app
 import xlrd
 
-UPLOAD_FOLDER =r'C:\Users\Administrator\Desktop\script\flask\microblog'
+UPLOAD_FOLDER =r'C:\Users\Administrator\Desktop\script\flask\microblog\app\static\Uploads'
 def getData(sheetName):
-    book = xlrd.open_workbook(r'data.xlsx')
+    book = xlrd.open_workbook(r'C:\Users\Administrator\Desktop\script\flask\microblog\app\static\Uploads\data.xlsx')
     sh = book.sheet_by_name(sheetName)
     num_rows = sh.nrows
     num_cols = sh.ncols
