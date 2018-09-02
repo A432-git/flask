5f7f79bf106d736096a90a0e34a3ce50552b17ee
yangr5 <yangtao584@126.com>
2018-09-02 20:55:32 +0800

    add js for table edit in test

--- test/app/views.py
+++ test/app/views.py
@@ -3,29 +3,16 @@ from flask import Flask, request,jsonify, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from app import app
 import xlrd
-UPLOAD_FOLDER =os.path.join(os.path.dirname(os.path.abspath(__file__)),'Uploads')
-DATA = os.path.join(UPLOAD_FOLDER,'data.xlsx')
-allPages = {}
-def initData():
-    book = xlrd.open_workbook(DATA)
-    sheetNames = book.sheet_names()
-    allPages['keys'] = sheetNames
+
+
+
 
 
 @app.route('/')
-@app.route('/home/')
+@app.route('/datatable')
 def home():
-    initData()
-    return render_template("Home.html",links=allPages['keys'])
+    return render_template("datatable.html")
     
 
-    
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method == 'GET':
-        return render_template('manage.html')
-    elif request.method == 'POST':
-        # request_data = request.get_json()
-        keys=request.json.get('test')
-        return jsonify({'length':len(keys),'data':keys[1]})
+
 
