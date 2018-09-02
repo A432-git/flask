648711f197fc1388ee3eecd33fcf8a5ae5cff8fb
yangr5 <yangtao584@126.com>
2018-09-02 22:55:54 +0800

    add js for table edit in test.

--- test/app/views.py
+++ test/app/views.py
@@ -3,7 +3,7 @@ from flask import Flask, request,jsonify, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from app import app
 import xlrd
-
+import time
 
 
 
@@ -12,6 +12,16 @@ import xlrd
 @app.route('/datatable')
 def home():
     return render_template("datatable.html")
+
+@app.route('/test')
+def test():
+    return render_template("ajax_loading.html")
+
+
+@app.route('/example')
+def example():
+    time.sleep(3)
+    return jsonify("I am ajax!")
     
 
 
