56ebc223bd04805c5d6bd9f3aa5adcead5c6c50f
yangr5 <yangtao584@126.com>
2019-01-15 20:00:37 +0800

    remove warning for lab management

--- dm_lab_with_flask_admin/app.py
+++ dm_lab_with_flask_admin/app.py
@@ -4,16 +4,7 @@ from app.model import User, Storage, Host, OperationSystem, RigConnection, Rig
 import os
 
 from werkzeug.security import generate_password_hash, check_password_hash
-# Create dummy secrey key so we can use sessions
-app.config['SECRET_KEY'] = '123456790'
 
-# Create in-memory database
-app.config['DATABASE_FILE'] = '../data/spe_djy_lab.sqlite'
-# this is for sqlite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
-# this is for mysql
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@Baidu123@localhost:3306/test'
-app.config['SQLALCHEMY_ECHO'] = True
 
 
 # Flask views
@@ -51,4 +42,4 @@ def rest_for_jia(obj_name, name, obj_property, obj_value):
 
 if __name__ == '__main__':
     # Start app
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- dm_lab_with_flask_admin/app/__init__.py
+++ dm_lab_with_flask_admin/app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+
+# Create in-memory database
+app.config['DATABASE_FILE'] = '../data/spe_djy_lab.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# this is for sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+# this is for mysql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@Baidu123@localhost:3306/test'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 # set optional bootswatch theme
 
